d22e0942ba7b2f955236c590f9f8d42d398bcb5e
verabradu <verabradu92@gmail.com>
2025-06-03 00:17:15 -0500

    Implemented Student entity with annotations, constructor, getters and setters

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -1,13 +1,19 @@
 package pixel.academy.crud_app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudAppApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(CrudAppApplication.class, args);}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner ->{
+			System.out.println("Hello, World!");
+		};
+	}
 }
