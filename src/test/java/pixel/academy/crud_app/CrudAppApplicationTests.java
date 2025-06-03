47a411a0e58940a7a62ee326bc63a29f8264fb76
verabradu <verabradu92@gmail.com>
2025-06-02 23:09:07 -0500

    Initial commit

---
+++ src/test/java/pixel/academy/crud_app/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
